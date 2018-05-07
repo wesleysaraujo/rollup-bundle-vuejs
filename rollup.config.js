8567a7dfd2feddce1bc4896959a0b5d8926b25b9
Wesley Serafim de Araújo <wesley@eguana.com.br>
2018-05-07 11:59:30 -0300

    Components VueJs to use standalone

---
+++ rollup.config.js
@@ -0,0 +1,37 @@
+import vue from 'rollup-plugin-vue'
+import buble from 'rollup-plugin-buble'
+import nodeResolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
+import uglify from 'rollup-plugin-uglify'
+import livereload from 'rollup-plugin-livereload'
+import serve from 'rollup-plugin-serve'
+
+const config = {
+    entry: 'src/main.js',
+    dest: 'dist/assets/js/build.js',
+    format: 'iife',
+    sourceMap: true,
+    useStrict: true,
+    plugins: [
+        vue({ compileTemplate: true, css: 'dist/assets/css/build.css' }),
+        buble({ exclude: 'node_modules/**' }),
+        nodeResolve({ browser: true, jsnext: true }),
+        commonjs()
+    ]
+}
+
+if (process.env.NODE_ENV === 'production') {
+    config.sourceMap = false
+    config.plugins.push(uglify())
+}
+
+if (process.env.NODE_ENV === 'development') {
+    config.plugins.push(livereload())
+    config.plugins.push(serve({
+        contentBase: './',
+        port: 8088,
+        open: false
+    }))
+}
+
+export default config
---
+++ src/main.js
@@ -0,0 +1,11 @@
+import Vue from 'vue/dist/vue.js'
+import App from './App.vue'
+import Todo from './components/todo/Main.vue'
+
+new Vue({
+    el: '#app',
+    components: {
+      App,
+      Todo
+    }
+})
